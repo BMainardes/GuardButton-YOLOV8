77aac5e1e071d1ded4d17496b28c086247f225fb
Bruno Vieira <143436749+BMainardes@users.noreply.github.com>
2024-08-20 19:31:47 -0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,57 +1,40 @@
-# Este script tem como função criar o dataset que será utlizado como banco de imagens para a rede YOLOV8 
+# Este script tem como função adicionar imagens de alta qualidade 
+# para a criação do dataset que será utilizado no YOLOV8 
 
-# A API utilizada é da Unsplash, uma vez que a biblioteca DuckDuckGoImages parece apresentar mal funcionamento em versões mais recentes do Python ,
-# para usar o script, digitar o seguinte comando no Prompt de Comando
-# pip install requests
-# Feito por Bruno Gabriel Mainardes Vieira, estudante de Análise e Desenvolvimento de Sistemas ,
+# Para o funcionamento da biblioteca de imagens do bing, usar o seguinte comando no Prompt de Comando
+# pip install bing-image-downloader
 
-import requests
+# encoding: utf-8
+# Python ver 3.8.0
+# Criado por Bruno Gabriel Mainardes Vieira
+
+from bing_image_downloader import downloader
 import os
-from urllib.parse import urlparse
 
-# Unsplash API key
-# Registre-se em https://unsplash.com, pegue sua API key e insira ela aqui
-access_key = " " 
+# Definição dos parâmetros e indicação do diretório do dataset
+keyword = 'gun'
+output_dir = 'C:/Users/Bruno/Desktop/GuardButton-YOLOV8/dataset/images'
+limit = 100  # Numero de imagens a ser baixada
 
-# Definindo o filtro de busca e o diretório de destino
-filtro = 'gun'
-destino = 'C:\\Users\\Bruno\\Desktop\\GuardButton alpha0.0.1\\dataset'
+# Criação do diretório de imagens caso o mesmo não exista
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-# Criar o diretório de destino se não existir
-if not os.path.exists(destino):
-    os.makedirs(destino)
+# Download das imagens e filtros
+def safe_download():
+    try:
+        downloader.download(
+            keyword,
+            limit=limit,            # Limite de imagens
+            output_dir=output_dir,  # Diretório de download
+            adult_filter_off=True,  # Usado para evitar conteúdo explícito
+            force_replace=False,    
+            timeout=60              # Limite tempo de operação
+        )
+        print(f'Images downloaded to {output_dir}')
 
-# Filtro e download das imagens
-def download_image(url, folder, file_name):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(os.path.join(folder, file_name), 'wb') as file:
-            file.write(response.content)
-    else:
-        print(f"Falha ao baixar a imagem: {url}")
+        #Depuração Adicionada para erros
+    except Exception as e:
+        print(f'An error occurred: {e}')
 
-print("Iniciando downloads...")
-
-try:
-    # URL da API de busca de imagens do Unsplash
-    url = "https://api.unsplash.com/search/photos"
-    params = {
-        "query": filtro,
-        "per_page": 10,  # Editar este parâmetro para que o número de imagens seja maior
-        "client_id": access_key
-    }
-    
-    response = requests.get(url, params=params)
-    data = response.json()
-
-    # Baixar imagens
-    for i, image in enumerate(data['results']):
-        image_url = image['urls']['regular']
-        file_name = f"image_{i+1}.jpg"
-        download_image(image_url, destino, file_name)
-
-except Exception as e:
-    # Depuração
-    print("Erro:", str(e))
-
-print('Downloads Concluídos :D')
+safe_download()
